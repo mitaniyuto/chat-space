d89b6478c5aba83007717de7e668d51dd20cdbff
YourName <you@example.com>
2019-11-30 15:40:52 +0900

    Revert "group mk"
    This reverts commit 6c60ba135fccf3796325678bfc0707950a2cae60.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
-  end
-  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root to: "messages#index"
 end
