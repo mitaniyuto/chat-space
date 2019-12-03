edcb6e30adbe8ea948678ecbaa92b1a0db51eb4b
YourName <you@example.com>
2019-12-03 11:26:05 +0900

    message send

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,8 +17,23 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+       redirect_to group_messages_path(@group)
+    else
+      render :edit
+    end
+  end
+  
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group =Group.find(params[:id])
   end
 end
