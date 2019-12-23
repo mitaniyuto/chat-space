f404b97716c2045016a7d8031179e2a37ad56e94
YourName <you@example.com>
2019-12-23 20:50:55 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -21,8 +21,31 @@ $(function () {
           </div>
         </div>`
     return html;
-  }
-  }) 
+  } 
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});      
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('メッセージ送信に失敗しました');
+    })
+  })
+
    
   var reloadMessages = function () {
     last_message_id = $('.message:last').data("message-id"); 
@@ -48,3 +71,4 @@ $(function () {
       });
     }
   
+  });
